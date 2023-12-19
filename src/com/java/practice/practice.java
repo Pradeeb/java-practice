94269c3bcd3fed1d920fccc9c0ead2e604ee029f
Your Name <your.email@example.com>
2023-12-19 16:25:23 +0530

    division in given number digit wise

--- src/com/java/practice/practice.java
+++ src/com/java/practice/practice.java
@@ -1,39 +1,37 @@
 package com.java.practice;
 
-import java.io.IOException;
+import java.util.Arrays;
 
 
 public class practice {
+	
 
-	public static void main(String[] args) throws IOException {
-		System.out.println("test");
-		
-		practice ob = new practice();
-		System.out.println(ob.getLastDigit("3", "10"));
+	// Driver program to run the case
+	public static void main(String args[])
+	{
+	    int N = 22074568;
+	    System.out.println(evenlyDivides(N));
 	}
 	
-	protected int getLastDigit(String a, String b) {
-	      //check minimun leve case
-	      if(a.equals("1")) return 1;
-	      
-	      if(b.length()==1 && b.equals("0")) return 1;
-	      
-	      // Now check the modular to b
-	       int pm=getModularINb(4,b);
-	       int exp= pm==0 ?4 :pm;
-	       
-	       char ch=a.charAt(a.length()-1);
-	       long ans=(long) Math.pow(ch-'0',exp);
-	       
-	       return (int)ans%10;
-	    }
-	    protected int getModularINb(int a, String b){
-	        
-	        int res=0;
-	        for(int i=0;i<b.length() ;i++){
-	            res=((res*10)+(b.charAt(i)-'0'))%a;
-	        }
-	        return res;
-	    }
-
+	protected static int evenlyDivides(int N) {
+		
+		int tem=N,ans=0;
+		
+		while(tem !=0) {
+			
+			//get the last digit
+			int d=tem%10;
+			
+			//remove last digit in tem
+			tem /=10;
+			
+			if(d >0 && (N%d ==0)) {
+				ans++;
+			}
+			
+		}
+		
+		return ans;
+	} 
+		
 }
