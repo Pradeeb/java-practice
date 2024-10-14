db9a0bb800eaf92fddaa3d64252a4d68bead6af6
pradeeb <valanpradeep6@gmail.com>
2024-10-14 16:01:22 +0530

    test fihe have a modified

--- src/com/java/practice/Test.java
+++ src/com/java/practice/Test.java
@@ -3,37 +3,9 @@ package com.java.practice;
 public class Test {
 	public static void main(String[] args) {
 		
-		StringBuilder sb = new StringBuilder("Initial");
-
-        // Create two threads that will modify the StringBuilder
-        Thread thread1 = new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
-                sb.append("A");
-                System.out.println("Thread 1: " + sb.toString());
-            }
-        });
-
-        Thread thread2 = new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
-                sb.append("B");
-                System.err.println("Thread 2: " + sb.toString());
-            }
-        });
-
-        // Start both threads
-        thread1.start();
-        thread2.start();
-        
-        try {
-            thread1.join();
-            thread2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Final result
-        System.out.println("Final StringBuilder content: " + sb.toString());
-        
+		for(int i=0; i<=5 ; i++) {
+			System.out.println(i);
+		}
 	}
 }
 
