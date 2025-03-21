5e05b26913bb51fc9b21f2ef3104cbd6b8a5c519
pradeeb <valanpradeep6@gmail.com>
2025-03-21 16:41:37 +0530

    new branch create

--- src/com/java/practice/multithreas/LearnMultithread.java
+++ src/com/java/practice/multithreas/LearnMultithread.java
@@ -2,54 +2,7 @@ package com.java.practice.multithreas;
 
 // It is using for concurrent
 public class LearnMultithread {
-
 	public static void main(String[] args) {
 		
-		for( int i=0  ; i<4 ;i++) {
-			
-		//use Extends
-		Cake redWelvet=new Cake();
-		redWelvet.start();
-		
-		//use implements
-		Cake1 blackForast=new Cake1();
-		Thread thread=new Thread(blackForast);
-		thread.start();
-		}
-	}
-
-}
-
-// you can not use multiple inheritance here
-class Cake extends Thread {
-	
-	public void run() {
-		try {
-		System.out.println("Mixing for cake "+Thread.currentThread().getId());
-		Thread.sleep(100);
-		System.out.println("Baking cake "+Thread.currentThread().getId());
-		Thread.sleep(100);
-		System.out.println("Decorate cake "+Thread.currentThread().getId());
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-		
-}
-
-//you can use multiple inheritance here
-class Cake1 implements Runnable {
-	public void run() {
-		try {
-		System.out.println("Mixing for cake "+Thread.currentThread().getId());
-		Thread.sleep(100);
-		System.out.println("Baking cake "+Thread.currentThread().getId());
-		Thread.sleep(100);
-		System.out.println("Decorate cake "+Thread.currentThread().getId());
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 }
