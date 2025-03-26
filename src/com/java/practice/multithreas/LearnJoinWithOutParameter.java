0b4531136982f61081c70dc8a65c3f2e808c2056
pradeeb <valanpradeep6@gmail.com>
2025-03-26 15:50:08 +0530

    Learn Thread join method

---
+++ src/com/java/practice/multithreas/LearnJoinWithOutParameter.java
@@ -0,0 +1,31 @@
+package com.java.practice.multithreas;
+
+
+public class LearnJoinWithOutParameter {
+
+	public static void main(String[] args) {
+		
+		Runnable run= () -> {
+			for(int i=0 ; i<=5 ; i++) {
+			
+				System.out.println(i+" "+Thread.currentThread().getName());
+			}
+		};
+		
+		Thread thread1=new Thread(run);
+		Thread thread2=new Thread(run);
+		
+		thread1.start();
+		thread2.start();
+		
+		try {
+            thread1.join(); // Wait for thread1 to finish
+            thread2.join(); // Wait for thread2 to finish
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+		
+		System.out.println("completed");
+	}
+
+}
