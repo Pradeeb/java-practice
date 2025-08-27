b8c586a4739f332388f4d6853f8d52357d14c900
Pradeeb <valanpradeep6@gmail.com>
2025-08-27 16:29:19 +0530

    push

--- src/com/java/learn/dsa/Test.java
+++ src/com/java/learn/dsa/Test.java
@@ -4,16 +4,22 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		LinkedList02 list=new LinkedList02();
+		LinkedList02<Integer> list=new LinkedList02<Integer>();
 		
 		list.addBeginning(5);
 		list.addBeginning(6);
 		list.addBeginning(4);
 		
 		list.insert(3,10);
-		
+		list.display();
+		System.out.println("");
+	//	list.deleteAtPossition(5);
 		list.display();
 		
+		for(int a:list) {
+			System.out.println(a);
+		}
+
 	}
 
 }
