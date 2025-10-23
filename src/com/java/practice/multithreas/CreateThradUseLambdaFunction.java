14ff129a61d9e1c9ceda132971c661acaab95180
Pradeeb <valanpradeep6@gmail.com>
2025-10-23 12:58:10 +0530

    thread parameeter add learn

--- src/com/java/practice/multithreas/CreateThradUseLambdaFunction.java
+++ src/com/java/practice/multithreas/CreateThradUseLambdaFunction.java
@@ -1,5 +1,8 @@
 package com.java.practice.multithreas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CreateThradUseLambdaFunction {
 
 	public static void main(String[] args) {
@@ -8,8 +11,29 @@ public class CreateThradUseLambdaFunction {
 			System.out.println("Thread Creadted Use lambda Function");
 		};
 		
+		
+		
 		Thread thread=new Thread(run);
 		thread.start();
+		
+		// above regular way 
+		// parameter pass here
+		
+	      List<String> filePaths = new ArrayList<>();
+	        for (int i = 1; i <= 1000; i++) {
+	            filePaths.add("C:/files/file" + i + ".txt");
+	        }
+	        for (String path : filePaths) {
+	        	 String threadName = "FileThread-" + path.substring(path.lastIndexOf("/") + 1); // or any unique part
+	            Thread t = new Thread(() -> creataFile(path),threadName);
+	            t.start();
+	        }
+
+	
+	}
+	
+	static void creataFile(String i) {
+		System.out.println(Thread.currentThread().getName() + " processing " + i);
 	}
 
 }
--- src/com/java/practice/streams/Test.java
+++ src/com/java/practice/streams/Test.java
@@ -2,8 +2,7 @@ package com.java.practice.streams;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 public class Test {
 
