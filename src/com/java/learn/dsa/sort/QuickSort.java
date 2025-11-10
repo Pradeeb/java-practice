25f499715b60d2f83b99f96d970bcd7b5a463424
Pradeeb <valanpradeep6@gmail.com>
2025-11-10 17:12:33 +0530

    quick sort code

---
+++ src/com/java/learn/dsa/sort/QuickSort.java
@@ -0,0 +1,46 @@
+package com.java.learn.dsa.sort;
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+		
+		int[] arr= {1,5,7,6,2,8,4,3,9,1};
+		quickSort(arr,0,arr.length-1);
+		
+		System.out.println(Arrays.toString(arr));
+	
+	}
+	
+	public static void quickSort(int[] arr, int low, int heig) {
+		
+		if(low >= heig) {
+			return;
+		}
+		int start=low,end=heig;
+		
+		int mid=(low+heig)/2;
+		int pivot=arr[mid];
+		
+		while(start<=end) {
+			while(arr[start]<pivot) {
+				start++;
+			}
+			while(arr[end]>pivot) {
+				end--;
+			}
+			if(start<=end) {
+				int temp =arr[start];
+				arr[start]=arr[end];
+				arr[end] =temp;
+				start++;
+				end--;
+			}
+		}
+		
+		quickSort(arr,low,end);
+		quickSort(arr,start,heig);
+	}
+
+}
