e7f8fbdc571b106c8fab692262b29f4767d0c78f
Pradeeb <valanpradeep6@gmail.com>
2025-11-26 16:36:11 +0530

    java use find all duplicate elements in given string

---
+++ src/com/java/practice/youtube/Q1.java
@@ -0,0 +1,25 @@
+package com.java.practice.youtube;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+// java program to count the occurrence of the each character in a string
+public class Q1 {
+	
+	public static void main(String[] args) {
+		System.out.println("Enter the word :");
+		Scanner sc=new Scanner(System.in);
+		String text=sc.nextLine();
+		
+		Map<String, List<String>> map = Arrays.stream(text.split("")).collect(Collectors.groupingBy(s->s));
+		System.out.println(map);
+		
+		 Map<String, Long> ans = Arrays.stream(text.split("")).collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+		System.out.println(ans);
+	}
+
+}
