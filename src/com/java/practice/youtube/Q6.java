6986439588ae112a4a8d989ad86ceb0f677841b1
Pradeeb <valanpradeep6@gmail.com>
2025-11-27 15:11:22 +0530

    Find the first character match the element

---
+++ src/com/java/practice/youtube/Q6.java
@@ -0,0 +1,23 @@
+package com.java.practice.youtube;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+// Find the first character match the element
+public class Q6 {
+
+	public static void main(String[] args) {
+		
+		int[] num= {1,2,3,5,10,11,21,13};
+		
+		List<Integer> ans=Arrays.stream(num).mapToObj(String::valueOf)
+				                .filter(x-> x.startsWith("1"))
+				                .map(Integer::valueOf)
+				                .collect(Collectors.toList());
+		
+		System.out.println(ans);
+
+	}
+
+}
