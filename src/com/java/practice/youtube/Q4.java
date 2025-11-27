e05eb36698c487d3c1d7dae5b9f3e795278cd02a
Pradeeb <valanpradeep6@gmail.com>
2025-11-27 11:52:38 +0530

    Fins the second maximum number

---
+++ src/com/java/practice/youtube/Q4.java
@@ -0,0 +1,21 @@
+package com.java.practice.youtube;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+
+// Fins the second maximum number
+public class Q4 {
+
+	public static void main(String[] args) {
+
+		int[] num= {1,5,8,7,9,2,10,32,21};
+		
+		  Integer ans = Arrays.stream(num).boxed()  // its use to convert int to Integer 
+				 .sorted(Comparator.reverseOrder()) // if you want ASCR remove => Comparator.reverseOrder()
+				 .skip(1)
+				 .findFirst()
+				 .get();
+		 System.out.println(ans);
+	}
+}
