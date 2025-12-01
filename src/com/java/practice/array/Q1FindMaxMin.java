a2f404eebc8bcab3922221fecab0b81e75d03445
Pradeeb <valanpradeep6@gmail.com>
2025-12-01 16:26:02 +0530

    compare array stream add

--- src/com/java/practice/array/Q1FindMaxMin.java
+++ src/com/java/practice/array/Q1FindMaxMin.java
@@ -28,11 +28,23 @@ public class Q1FindMaxMin {
 		
 		//find maximum,minimum number method 1
 		Arrays.sort(numbers);
-		
 		min=numbers[0];
 		max=numbers[numbers.length-1];
+		System.out.println(min+"  "+max);
+
+		//find max value use stream
+		System.out.println("find max value use stream => "+ Arrays.stream(numbers).max().getAsInt());
 		
-		System.out.print(min+"  "+max);
+		//find min value use stream
+		System.out.println("find min value use stream => "+Arrays.stream(numbers).min().getAsInt());
+		
+		//find second minimum value use stream
+		System.out.println("find second minimum value use stream => "+Arrays.stream(numbers).sorted().skip(1).min().getAsInt());
+		
+		//find second max value use stream
+		System.out.println("find second max value use stream => "+Arrays.stream(numbers).sorted().skip(1).max().getAsInt());
+		
+
 		
 	}
 }
