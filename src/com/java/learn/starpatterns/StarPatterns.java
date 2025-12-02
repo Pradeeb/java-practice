bc3c7cac2d089b75eb81b4dcef2d82be2b20951c
Pradeeb <valanpradeep6@gmail.com>
2025-12-02 12:16:17 +0530

    dimond patern

--- src/com/java/learn/starpatterns/StarPatterns.java
+++ src/com/java/learn/starpatterns/StarPatterns.java
@@ -90,7 +90,31 @@ public class StarPatterns {
 			}
 			System.out.println("");
 		}
+		
+		
+		System.out.println("");
+		System.out.println("Pattern 5 dimand");
+		
+		 for (int i = 1; i <= n; i++) {
+		        for (int j = i; j < n; j++) {
+		            System.out.print(" ");
+		        }
+		        for (int j = 1; j <= (2 * i - 1); j++) {
+		            System.out.print("*");
+		        }
+		        System.out.println();
+		    }
 
+		    // Lower half
+		    for (int i = n - 1; i >= 1; i--) {
+		        for (int j = n; j > i; j--) {
+		            System.out.print(" ");
+		        }
+		        for (int j = 1; j <= (2 * i - 1); j++) {
+		            System.out.print("*");
+		        }
+		        System.out.println();
+		    }
 	}
 
 }
