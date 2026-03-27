a900ecad088690e6123d77c174dd144a80fd01cd
Pradeeb <valanpradeep6@gmail.com>
2026-03-27 16:57:02 +0530

    practice

--- src/com/java/test/Test.java
+++ src/com/java/test/Test.java
@@ -1,32 +1,26 @@
 package com.java.test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Test{
 	public static void main(String[] args) {
 		
-		List<Integer> nums=new ArrayList<>();
-
-		//create
-		nums.add(1);
-		nums.add(2);
-		nums.add(3);
-		nums.add(4);
-		nums.add(5);
+		int[] num= {1,2,3,4,5,6,8,4};
+		int target=8;
 		
-		//insert
-		nums.add(4, 10);;
-		System.out.println(nums);
+		Map<Integer,Integer> map=new HashMap<>();
 		
-		//update
-		nums.set(4, 11);;
-		System.out.println(nums);
-		
-		nums.remove(11);
-		System.out.println(nums);
-
-		
+		for(int i=0 ; i < num.length ;i++){
+			int complement=target-num[i];
+			
+			if(map.containsKey(complement)) {
+				System.out.println("Index{}:"+map.get(complement)+","+i);
+				break;
+			}else {
+				map.put(num[i], i);
+			}
+		}
 		
 	}
 }
