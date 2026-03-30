b940f7e7db2cf2f2c8666ea3fc4959ecc1911e4a
Pradeeb <valanpradeep6@gmail.com>
2026-03-30 12:20:00 +0530

    java 8 Optional class future

---
+++ src/com/java/learn/java8/OptionalClass/OptionalClassLearn.java
@@ -0,0 +1,61 @@
+package com.java.learn.java8.OptionalClass;
+
+import java.util.Optional;
+
+/*
+ * 1. of(value)                    => If value is null through null pointer exception
+ * 2. ofNullable(value)            => It can accept null 
+ * 3. isPresent()                  => If a value is present, returns true, otherwise false
+ * 4. isEmpty()                    => If a value is not present, returns true, otherwise false ( java 11 )*
+ * 5. get()                        => If a value is present, returns the value, otherwise throws NoSuchElementException.
+ * 6. orElse("default")            => If a value is present, returns the value, otherwise returns other.
+ * 7. orElseGet(()->"dumy"))       => If a value is present, returns the value, otherwise returns the resultproduced by the supplying function.
+ * 
+ * 8. orElseThrow(Supplier<? extends String> supplier)  
+ * 9. orElseThrow(Supplier<? extends RuntimeException> exceptionSupplier)
+ * 10. isPresent(x->syso())
+ * 11. filter()
+ * */
+
+public class OptionalClassLearn {
+
+	public static void main(String[] args) {
+		
+		String name="valan";
+		
+		// 1. of(value)    
+		Optional<String> opt=Optional.of(name);
+		System.out.println("1. "+opt.get());
+		
+		//2. ofNullable(value)
+		System.out.println("2. "+Optional.ofNullable(name));
+		
+		//3. isPresent()  
+		System.out.println("3. "+Optional.ofNullable(name).isPresent());
+		
+		//4. isEmpty()  
+		System.out.println("4. "+Optional.ofNullable(name).isEmpty());
+		
+		//5. get()  
+		System.out.println("5. "+Optional.ofNullable(name).get());
+		
+		//6. orElse()  
+		System.out.println("6. "+Optional.ofNullable(name).orElse("dumy"));
+		
+		//7. orElseGet()  
+	    System.out.println("7. "+Optional.ofNullable(name).orElseGet(()->"dumy"));
+	    
+		//8. orElseThrow(Supplier<? extends String> supplier)  
+	    System.out.println("8. "+Optional.ofNullable(name).orElseGet(()->"dumy"));
+	    
+	    //9. orElseThrow(Supplier<? extends RuntimeException> exceptionSupplier)
+	    String o = Optional.ofNullable(name).orElseThrow(()->new RuntimeException());
+	    
+	    //10. map()
+	    System.out.println("10. "+Optional.ofNullable(name).map(String::toUpperCase).get());
+	    
+	    //11. filter()
+	    System.out.println("11. "+Optional.ofNullable(name).filter(x->x.startsWith("v")).get());
+	    
+	}
+}
--- src/com/java/test/Test.java
+++ src/com/java/test/Test.java
@@ -1,26 +1,17 @@
 package com.java.test;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Test{
 	public static void main(String[] args) {
 		
 		int[] num= {1,2,3,4,5,6,8,4};
-		int target=8;
 		
-		Map<Integer,Integer> map=new HashMap<>();
+		int min=Integer.MAX_VALUE;
+		int profit=0;
 		
-		for(int i=0 ; i < num.length ;i++){
-			int complement=target-num[i];
-			
-			if(map.containsKey(complement)) {
-				System.out.println("Index{}:"+map.get(complement)+","+i);
-				break;
-			}else {
-				map.put(num[i], i);
+		for(int price:num) {
+			if(min>price) {
+				
 			}
 		}
-		
 	}
 }
