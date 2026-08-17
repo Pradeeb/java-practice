307311542bb16157ea1586f97ac9f9445b8d6cab
Pradeeb <valanpradeep6@gmail.com>
2026-08-17 17:13:08 +0530

    custom annotaion

--- src/com/java/learn/CustomAnnotation/LearnCustomAnnotaion.java
+++ src/com/java/learn/CustomAnnotation/LearnCustomAnnotaion.java
@@ -4,17 +4,23 @@ public class LearnCustomAnnotaion {
 
     @OnlyNumber
     private String mobile;
+    
+    @NoXSS
+    private String name;
 
-    public LearnCustomAnnotaion(String mobile) {
+    public LearnCustomAnnotaion(String mobile,String name) {
         this.mobile = mobile;
+        this.name = name;
     }
 
     public static void main(String[] args) {
 
-        LearnCustomAnnotaion obj = new LearnCustomAnnotaion("valan");
+        LearnCustomAnnotaion obj = new LearnCustomAnnotaion("1","valan");
+        LearnCustomAnnotaion obj2 = new LearnCustomAnnotaion("1","<script>alert('XSS')</script>");
 
         try {
             Validator.Validate(obj);
+            Validator.ValidateNoXSS(obj2);
         } catch (Exception e) {
             e.printStackTrace();
         }
