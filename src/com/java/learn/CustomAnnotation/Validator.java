20bbdb3df2b2870b59385afe0f270f55bca27c9a
Pradeeb <bgdit@bhavishgraphics.com>
2026-08-17 17:13:08 +0530

    custom annotaion

--- src/com/java/learn/CustomAnnotation/Validator.java
+++ src/com/java/learn/CustomAnnotation/Validator.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Field;
 
 public class Validator {
 
-    public static void Validate(Object obj) throws Exception {
+    public static void Validate(Object obj) throws Exception{
 
         Field[] fieldList = obj.getClass().getDeclaredFields();
 
@@ -20,6 +20,28 @@ public class Validator {
                     throw new Exception("Field " + field.getName() + " must contain only numbers");
                 }
             }
+ 
         }
+    }
+    
+    public static void ValidateNoXSS(Object obj) throws Exception {
+    	
+    	 Field[] fieldList = obj.getClass().getDeclaredFields();
+         for (Field field : fieldList) {
+             if (field.isAnnotationPresent(NoXSS.class)) {
+            	 field.setAccessible(true);
+                 String value = (String) field.get(obj);
+
+                 if (value != null &&
+                     value.matches(".*<\\s*script.*>.*")) {
+
+                     throw new Exception(
+                         "XSS content detected in field: " + field.getName()
+                     );
+                 }
+             }
+         }
+        
+
     }
 }
